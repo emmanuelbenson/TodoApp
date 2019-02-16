507c30f2b1d90d71799b6b554f841cf468578a19
Emmanuel Benson <emmanuel.c.benson@gmail.com>
2019-02-16 06:10:02 +0100

    Added todo item component with complete functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
 import React, { Component } from "react";
+import Todos from "./components/Todos";
 import "./App.css";
 
 class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: "Take out trash",
+        completed: false
+      },
+      {
+        id: 2,
+        title: "Dinner with wife",
+        completed: false
+      },
+      {
+        id: 3,
+        title: "Meeting with boss",
+        completed: false
+      }
+    ]
+  };
+
+  markComplete = id => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    });
+  };
+
   render() {
     return (
       <div className="App">
-        <h1>App</h1>
+        <Todos todos={this.state.todos} markComplete={this.markComplete} />
       </div>
     );
   }
